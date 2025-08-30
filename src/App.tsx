e63bb86ebeef9af7273fd69b7e76b0bcadf09a74
James-Burch <burchjames6@gmail.com>
2025-08-30 19:09:59 +0100

    feat: add template page routing for individual templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Routes, Route } from "react-router-dom";
-import Layout from "./components/layout/Layout";
-import Home from "./pages/home";
-import Shop from "./pages/Shop";
-import Projects from "./pages/Projects";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Portal from "./pages/Portal";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import TemplatePage from './pages/TemplatePage';
+import Projects from './pages/Projects';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portal from './pages/Portal';
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/shop/:templateId" element={<TemplatePage />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
